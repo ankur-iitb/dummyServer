d74c4dbda0090700a7c2f3f3a1a4b4cb7ec8e0ab
Ankur <92906366+ankur-iitb@users.noreply.github.com>
2022-07-11 12:43:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-<?php
-$name = $_GET['name'];
-if ($name == null) {
-    $name = 'guest';
-}
-$message = $_GET['message'];
-if ($message == null) {
-    $message = 'hello there';
-}
-echo "$name says: $message";
+<html>
+    <body>
+        <?php
+        $name = $_GET['name'];
+        if ($name == null) {
+            $name = 'guest';
+        }
+        $message = $_GET['message'];
+        if ($message == null) {
+            $message = 'hello there';
+        }
+        echo "$name says: $message";
+        ?>
+    </body>
+</html>
